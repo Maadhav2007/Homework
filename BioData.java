3800904a2f93e946df81c4153774fa29f9a93674
maadhav <maadhavbalaji2007@gmail.com>
2026-02-26 18:52:29 +0530

    hii

---
+++ BioData.java
@@ -0,0 +1,14 @@
+class BioData
+{
+	public static void main (String[]args)
+	{
+	   String Name = "Ram";
+	   String Gender = "Male";
+	   String Address = "South Street,Madurai";
+	   long Pincode = 625706;
+	   long PhoneNumber = 7397670977l;
+	   System.out.println("My Name Is :"+ Name +  "Gender Is :"+Gender + "My Address Is:"+ Address+ " Pincode Is:"+ Pincode  + " My Number Is:"+PhoneNumber);
+	   	
+	}
+	
+}
---
+++ mobile.java
@@ -0,0 +1,12 @@
+class mobile
+{
+  public static void main(String[]args)
+    {
+	   String MobailName= iPhone 15 Pro;
+	   String MobileBrand=Apple;
+       float ScreenSize=6.1f;
+	   short BatteryCapacity=3274
+	   double Price=134999.99
+	   System.out.println("MobailName" "MobileBrand" "ScreenSize" "BatteryCapacity" "Price")
+	}
+}
