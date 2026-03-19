9490f8fe9e3b115a8574a1e8b7b8c6c79eb711b0
maadhav <maadhavbalaji2007@gmail.com>
2026-03-19 15:52:36 +0530

    hii

---
+++ Array1.java
@@ -0,0 +1,14 @@
+class Array1
+{
+	public static void main(String[] args)
+	{
+		String n[]=new String[3];
+		n[0]="NARUTO";
+		n[1]="TANJIRO";
+		n[2]="LUFFY";
+		for(int i=0;i<n.length;i++)
+		{
+			System.out.println(n[i]);
+		}
+	}
+}
